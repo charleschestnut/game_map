0de28ca4dd85cfcad73b95d28970c8b5cc019afa
charleschestnut <carloscastcast97@gmail.com>
2020-07-28 16:27:04 +0200

    new: added the posibility of insert different types of square into the map ('MONSTER', 'START', and 'FINISH')

--- print_map/draw_map.py
+++ print_map/draw_map.py
@@ -1,7 +1,7 @@
 
 from draw_first_map import draw_square
 from round_squares import round_map
-
+from insert_types_square import insert_types_square
 
 vertical_walls = [1, 1]
 horizontal_walls_1 = [1, 0]
@@ -15,36 +15,16 @@ ROW_2 = 5
 
 VERTICAL_3 = [1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1]
 HORIZONTAL_3 = [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
-MONSTERS_3 = [0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1]
+DICT_TYPES_LIST_3 = {
+    "MONSTER": [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    "START": [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    "FINISH": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+}
 ROW_3 = 5
 COL_3 = 5
 
-# Método para el mapa
-def get_list_of_positions(list, rows, cols):
-    counter = 0
-    list_position = []
-    for l in list:
-        x = int(counter/rows)
-        y = counter % rows
-        list_position.append([x, y])
-        counter += 1
-    return list_position
 
-def get_square_of_map(map, x, y):
-    map_matrix = map.splitlines()
-    # Una casilla tiene 3x9 caracteres
-    square = ""
-    counter = 0
-    for line in map_matrix:
-        if 3*y <= counter < 3*(y+1):
-            square += line[9*x: 9*(x+1)]
-            square += '\n'
-        counter += 1
-    return square
-
-
-
-def print_map(horizontal_walls, vertical_walls, rows, cols):
+def print_map(horizontal_walls, vertical_walls, rows, cols, dict_types_square):
     map = ""
     for counter_row in range(rows):
         map_0 = ""
@@ -61,15 +41,10 @@ def print_map(horizontal_walls, vertical_walls, rows, cols):
         map_1 += '\n'
         map_2 += '\n'
         map += map_0 + map_1 + map_2
-    print(map)
     map_rounded = round_map(map, horizontal_walls, vertical_walls, rows, cols)
-    print(map_rounded)
+    map_typed = insert_types_square(map_rounded, dict_types_square, rows)
     return map_rounded
 
 
 #print_map(horizontal_walls_2, vertical_walls_2, row_2, col_2)
-map_1 = print_map(HORIZONTAL_3, VERTICAL_3, ROW_3, COL_3)
-square_0_0 = get_square_of_map(map_1, 0, 0)
-square_1_1 = get_square_of_map(map_1, 1, 1)
-print(square_1_1)
-print(square_0_0)
+print_map(HORIZONTAL_3, VERTICAL_3, ROW_3, COL_3, DICT_TYPES_LIST_3)
---
+++ print_map/insert_types_square.py
@@ -0,0 +1,54 @@
+def get_square_of_map(map, x, y, str_replace):
+    map_matrix = map.splitlines()
+    # Una casilla tiene 3x9 caracteres
+    new_map = ""
+    counter = 0
+    for line in range(len(map_matrix)):
+        if 3*y <= counter < 3*(y+1):
+            start = 9*x
+            end = 9*(x+1)
+            line_square = map_matrix[line][start:end]
+            line_square_edit = line_square.replace('   ', str_replace)
+            map_matrix[line] = map_matrix[line][:start] + line_square_edit + map_matrix[line][end:]
+        counter += 1
+
+    for row in map_matrix:
+        for c in row:
+            new_map += c
+        new_map += '\n'
+
+    return new_map
+
+
+def print_map_square_types(map, dict_type_list):
+    for type in dict_type_list:
+        for m in dict_type_list[type]:
+            if type == "MONSTER":
+                map = get_square_of_map(map, m[0], m[1], '|M|')
+            if type == "START":
+                map = get_square_of_map(map, m[0], m[1], '|S|')
+            if type == "FINISH":
+                map = get_square_of_map(map, m[0], m[1], '|F|')
+    print(map)
+    return map
+
+
+# Método para el mapa
+def get_list_of_positions(list, rows):
+    counter = 0
+    list_position = []
+    for l in list:
+        if l ==1 :
+            x = int(counter/rows)
+            y = counter % rows
+            list_position.append([x, y])
+        counter += 1
+    return list_position
+
+
+def insert_types_square(map, dict_types, rows):
+    new_dict = {}
+    for type in dict_types:
+        new_dict[type] = get_list_of_positions(dict_types[type], rows)
+    map_types_inserted = print_map_square_types(map, new_dict)
+    return map_types_inserted
