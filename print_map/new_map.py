c1325face2389b4cc5b0f4063ecdc268994c8ec2
charleschestnut <carloscastcast97@gmail.com>
2020-07-29 03:27:11 +0200

    fix: change squares dimensions from 2x2 to 3x2 for a beter visualization

--- print_map/new_map.py
+++ print_map/new_map.py
@@ -9,10 +9,11 @@ def insert_horizontal_walls(map_matrix, horizontal_walls, rows):
     for h_w in horizontal_walls:
         if h_w == 1:
             row = int(counter / rows) * 3
-            start = (counter % rows) * 3
-            end = start + 4
+            start = (counter % rows) * 4
+            end = start + 5
+            replace_count = 5
             line_wall = map_matrix[row][start: end]
-            line_wall_edit = line_wall.replace('...', '###')
+            line_wall_edit = line_wall.replace('.', '#', replace_count)
             map_matrix[row] = map_matrix[row][:start] + line_wall_edit + map_matrix[row][end:]
         counter += 1
     return map_matrix
@@ -24,7 +25,7 @@ def insert_vertical_walls(map_matrix, vertical_walls, cols):
     for v_w in vertical_walls:
         if v_w == 1:
             row = int(counter / (cols + 1)) * 3  # SI NO FUNCIONA CON OTRO MAPA, CAMBIAR COLS POR ROWS
-            start = (counter % (cols +1)) * 3
+            start = (counter % (cols +1)) * 4
             replace_count = 4
             for i in range(replace_count):
                 wall_character = '#'
@@ -48,14 +49,14 @@ def print_walls(map, horizontal_walls, vertical_walls, rows, cols):
 
 def print_basic_map(rows, cols):
 
-    length_cols = cols * 3 + 1
+    length_cols = cols * 4 + 1
     length_rows = rows * 3 + 1
 
     map = ''
     for r in range(length_rows):
         is_row_line = r == 0 or r % 3 == 0
         for c in range(length_cols):
-            is_col_line = c == 0 or c % 3 == 0
+            is_col_line = c == 0 or c % 4 == 0
             if is_row_line or is_col_line:
                 map += '.'
             else:
@@ -70,3 +71,5 @@ def create_complete_map(horizontal_walls, vertical_walls, rows, cols):
     #map_typed = print_types()
     return map_walled
 print(create_complete_map(HORIZONTAL_1, VERTICAL_1, ROWS_1, COLS_1))
+
+#print(print_basic_map(2, 2))
