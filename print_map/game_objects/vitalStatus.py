82ad416e15a32e03b0da95f1df5ac979da58573e
charleschestnut <carloscastcast97@gmail.com>
2020-08-25 18:29:17 +0200

    new: created new class called VitalStatus with it respective tests.

---
+++ print_map/game_objects/vitalStatus.py
@@ -0,0 +1,60 @@
+class VitalStatus:
+
+    def __init__(self, health, attack, magic_power, defense, magic_defense, status_proportion=None, extra_level=None):
+
+        for state in [health, attack, magic_power, defense, magic_defense]:
+            if not state:
+                raise Exception('The state can not be None.')
+            elif not isinstance(state, int):
+                raise Exception('The state must be an integer.')
+            elif state <= 0:
+                raise Exception('The state must be higher than zero.')
+
+        if status_proportion:
+            if len(status_proportion) != 5:
+                raise Exception(
+                    "Upgrade list must content 5 elements: hp, attack, magic power, defense and magic defense.")
+            for upgrade in status_proportion:
+                if not upgrade:
+                    raise Exception("Sorry, there's an upgrade that is None.")
+                elif isinstance(upgrade, int):
+                    raise Exception('The upgrades can only be floats.')
+                elif upgrade <= 0:
+                    raise Exception('The upgrades need to be greater than 0.0.')
+
+            self.status_proportion = status_proportion
+        else:
+            self.status_proportion = []
+
+        self.health = health
+        self.attack = attack
+        self.magic_power = magic_power
+        self.defense = defense
+        self.magic_defense = magic_defense
+        if extra_level:
+            self.extra_level = extra_level
+        else:
+            self.extra_level = 0
+
+    def __str__(self):
+        res = 'HP: '+str(self.health)+'\n'+'ATK: '\
+        +str(self.attack)+'\n'+'M. PWR: '\
+        +str(self.magic_power)+'\n'\
+        +'DEF: '+str(self.defense)+'\n'\
+        +'M.DEF: '+str(self.magic_defense)
+        return res
+
+    def get_vital_status_at_level(self, level):
+
+        for lvl in range(level-1):
+            self.health *= (1 + self.status_proportion[0])
+            self.attack *= (1 + self.status_proportion[1])
+            self.magic_power *= (1 + self.status_proportion[2])
+            self.defense *= (1 + self.status_proportion[3])
+            self.magic_defense *= (1 + self.status_proportion[4])
+        self.health = int(self.health)
+        self.attack = int(self.attack)
+        self.magic_power = int(self.magic_power)
+        self.defense = int(self.defense)
+        self.magic_defense = int(self.magic_defense)
+        return self
---
+++ print_map/tests/test_vitalStatus_management.py
@@ -0,0 +1,80 @@
+import unittest
+from print_map.game_objects import VitalStatus
+
+
+class MyTestCase(unittest.TestCase):
+    def test_single_vital_status(self):
+        status_proportion = [0.5, 0.5, 0.7, 0.9, 0.8]
+        extra_level = 1
+        v_status = VitalStatus(10, 3, 4, 6, 20, status_proportion, extra_level)
+        self.assertFalse(None, v_status)
+
+    def test_single_vital_status_default(self):
+        raises = False
+        try:
+            VitalStatus(10, 3, 4, 6, 20, None, 1)
+        except:
+            raises = True
+        self.assertFalse(raises)
+
+    def test_single_vital_status_len_four(self):
+        status_proportion = [0.5, 0.5, 0.7, 0.9]
+        self.assertRaises(Exception, VitalStatus, 10, 3, 4, 6, 20, status_proportion, 1)
+
+    def test_single_vital_status_len_six(self):
+        status_proportion = [0.5, 0.5, 0.7, 0.9, 0.8, 0.1]
+        self.assertRaises(Exception, VitalStatus, 10, 3, 4, 6, 20, status_proportion, 1)
+
+    def test_single_vital_status_none(self):
+        status_proportion = [0.5, 0.5, 0.7, 0.9, None]
+        self.assertRaises(Exception, VitalStatus, 10, 3, 4, 6, 20, status_proportion, 1)
+
+    def test_single_vital_status_integer(self):
+        status_proportion = [0.5, 0.5, 0.7, 0.9, 2]
+        self.assertRaises(Exception, VitalStatus, 10, 3, 4, 6, 20, status_proportion, 1)
+
+    def test_single_vital_status_negative(self):
+        status_proportion = [0.5, 0.5, 0.7, 0.9, -0.2]
+        self.assertRaises(Exception, VitalStatus, 10, 3, 4, 6, 20, status_proportion, 1)
+
+    def test_negative_extra_level(self):
+        status_proportion = [0.5, 0.5, 0.7, 0.9, 0.8]
+        v_status = VitalStatus(10, 3, 4, 6, 20, status_proportion, -1)
+        self.assertIsNotNone(v_status)
+
+    def test_negative_extra_level(self):
+        status_proportion = [0.5, 0.5, 0.7, 0.9, 0.8]
+        v_status = VitalStatus(10, 3, 4, 6, 20, status_proportion, -1)
+        self.assertIsNotNone(v_status)
+
+    def test_get_vital_status_level_one(self):
+        status_proportion = [0.5, 0.5, 0.7, 0.9, 0.8]
+        v_status = VitalStatus(10, 10, 10, 10, 10, status_proportion, -1)
+        status_level_one = v_status.get_vital_status_at_level(1)
+        self.assertEqual(v_status, status_level_one)
+
+    def test_get_vital_status_level_two(self):
+        status_proportion = [0.5, 0.5, 0.7, 0.9, 0.8]
+        v_status = VitalStatus(10, 10, 10, 10, 10, status_proportion, -1)
+        status_level_two = v_status.get_vital_status_at_level(2)
+        v_status_two = VitalStatus(15, 15, 17, 19, 18, status_proportion, -1)
+        self.assertEqual(v_status_two.health, status_level_two.health)
+        self.assertEqual(v_status_two.attack, status_level_two.attack)
+        self.assertEqual(v_status_two.magic_power, status_level_two.magic_power)
+        self.assertEqual(v_status_two.defense, status_level_two.defense)
+        self.assertEqual(v_status_two.magic_defense, status_level_two.magic_defense)
+
+    def test_get_vital_status_level_five(self):
+        status_proportion = [0.5, 0.5, 0.5, 0.5, 0.5]
+        v_status = VitalStatus(10, 10, 10, 10, 10, status_proportion, -1)
+        status_level_two = v_status.get_vital_status_at_level(5)
+        v_status_two = VitalStatus(50, 50, 50, 50, 50, status_proportion, -1)
+        self.assertEqual(v_status_two.health, status_level_two.health)
+        self.assertEqual(v_status_two.attack, status_level_two.attack)
+        self.assertEqual(v_status_two.magic_power, status_level_two.magic_power)
+        self.assertEqual(v_status_two.defense, status_level_two.defense)
+        self.assertEqual(v_status_two.magic_defense, status_level_two.magic_defense)
+
+
+if __name__ == '__main__':
+    unittest.main()
