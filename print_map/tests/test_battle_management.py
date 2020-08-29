e12b5a3f0a219abf001fb0cda9c0f34e6d7b3d81
charleschestnut <carloscastcast97@gmail.com>
2020-08-30 00:50:50 +0200

    fix: modified test with new game classes.

---
+++ print_map/tests/test_battle_management.py
@@ -0,0 +1,83 @@
+import unittest
+from print_map.game_objects import Battle, BoardMap, Character, Weapon, VitalStatus,\
+    Monster, Position, ConsoleInterface, Game
+
+SQUARES = [1, 1, 1, 1, 1, 1, 1, 1, 1,
+           1, 4, 0, 0, 0, 0, 0, 0, 1,
+           1, 0, 3, 1, 0, 1, 3, 0, 1,
+           1, 0, 2, 1, 0, 1, 1, 0, 1,
+           1, 0, 0, 0, 3, 0, 0, 0, 1,
+           1, 0, 1, 1, 0, 1, 1, 0, 1,
+           1, 0, 3, 1, 0, 2, 3, 0, 1,
+           1, 0, 0, 0, 0, 0, 0, 5, 1,
+           1, 1, 1, 1, 1, 1, 1, 1, 1]
+ROWS = 9
+COLS = 9
+
+status_proportion = [0.5, 0.5, 0.7, 0.9, 0.8]
+extra_level = 1
+
+v_status_character = VitalStatus(30, 5, 5, 5, 5, status_proportion, extra_level)
+v_status_monster_1 = VitalStatus(25, 4, 4, 4, 4, None, None)
+v_status_monster_2 = VitalStatus(100000, 2000, 3000, 5000, 5000, None, None)
+
+v_status_weapon_1 = VitalStatus(2, 3, -1, 5, 1, None, 0)
+v_status_weapon_2 = VitalStatus(2, 3, -2, 4, 1, None, 0)
+v_status_weapon_3 = VitalStatus(2, 3, 5, 5, 3, None, -1)
+v_status_weapon_4 = VitalStatus(2, 3, -1, 5, 1, None, 1)
+v_status_weapon_5 = VitalStatus(1, 1, 1, 1, 1, None, 0)
+
+weapon_1 = Weapon("Super sword", "This sword cuts what it wants.", v_status_weapon_1)
+weapon_2 = Weapon("Super shield", "This sword cuts what it wants.", v_status_weapon_2)
+weapon_3 = Weapon("Super hat", "This hat makes you invisible.", v_status_weapon_3)
+weapon_4 = Weapon("Super boots", "Those boots let you pass though poisoned rivers.", v_status_weapon_4)
+weapon_monster = Weapon("Super book", "This book increases your magic power.", v_status_weapon_5)
+
+character_name = "Onion Knight"
+monster_name_1 = "Gannondorf"
+monster_name_2 = "Dark link"
+uses_magic = True
+doesnt_use_magic = False
+
+board_map = BoardMap(ROWS, COLS)
+board_map.create_squares_of_boardmap(SQUARES)
+
+game = Game(board_map)
+
+monster_1 = Monster(monster_name_1, doesnt_use_magic, v_status_monster_1, weapon_monster)
+monster_2 = Monster(monster_name_2, doesnt_use_magic, v_status_monster_2, weapon_monster)
+
+character = Character(character_name, uses_magic, v_status_character)
+character.level = 4
+
+game.append_monster_list([monster_1, monster_2])
+game.append_character(character)
+game.append_weapon_list([weapon_1, weapon_2, weapon_3, weapon_4])
+
+
+class MyTestCase(unittest.TestCase):
+    def test_initiation_battle(self):
+        raises = False
+        try:
+            battle = Battle(character)
+        except:
+            raises = True
+        self.assertFalse(raises)
+
+    def test_battle_development(self):
+        raises = False
+        try:
+            battle = Battle(character)
+            has_won, acc = battle.realise()
+        except:
+            raises = True
+        self.assertFalse(raises)
+        if has_won:
+            self.assertSetEqual(set(game.characters_alive), set([character]))
+            self.assertEqual(character.level, 5)
+        else:
+            self.assertSetEqual(set(game.characters_alive), set([]))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ print_map/tests/test_game_management.py
@@ -0,0 +1,86 @@
+import unittest
+from print_map.game_objects import Game, BoardMap, VitalStatus, Monster, Weapon
+
+SQUARES = [1, 1, 1, 1, 1, 1, 1, 1, 1,
+           1, 4, 0, 0, 0, 0, 0, 0, 1,
+           1, 0, 3, 1, 0, 1, 3, 0, 1,
+           1, 0, 2, 1, 0, 1, 1, 0, 1,
+           1, 0, 0, 0, 3, 0, 0, 0, 1,
+           1, 0, 1, 1, 0, 1, 1, 0, 1,
+           1, 0, 3, 1, 0, 2, 3, 0, 1,
+           1, 0, 0, 0, 0, 0, 0, 5, 1,
+           1, 1, 1, 1, 1, 1, 1, 1, 1]
+ROWS = 9
+COLS = 9
+
+board_map = BoardMap(ROWS, COLS)
+board_map.create_squares_of_boardmap(SQUARES)
+
+game = Game(board_map)
+
+vital_status_monster = VitalStatus(10, 4, 3, 10, 3, None, None)
+vital_status_weapon = VitalStatus(1, 1, 1, 1, 1, None, None)
+
+weapon_monster = Weapon('Super sword', ' This sword cuts what it wants.', vital_status_weapon)
+monster_1 = Monster('Goomba', True, vital_status_monster, weapon_monster)
+monster_2 = Monster('Koopa', True, vital_status_monster, weapon_monster)
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_game_get_random_monster(self):
+        raises = False
+        try:
+            game.append_monster(monster_1)
+            game.append_monster(monster_2)
+            monster_random = game.get_random_monster()
+            print(monster_random)
+        except:
+            raises = True
+        self.assertFalse(raises)
+
+    def test_game_append_monster(self):
+        raises = False
+        try:
+            game.append_monster(monster_1)
+            game.append_monster(monster_2)
+        except:
+            raises = True
+        self.assertFalse(raises)
+
+    def test_game_append_monster_list(self):
+        raises = False
+        try:
+            game.append_monster_list([monster_1, monster_2])
+        except:
+            raises = True
+        self.assertFalse(raises)
+
+    def test_game_None_append_monster(self):
+        raises = False
+        try:
+            game.append_monster()
+        except:
+            raises = True
+        self.assertTrue(raises)
+
+    def test_game_None_append_monster(self):
+        raises = False
+        try:
+            board_map.append_monster()
+        except:
+            raises = True
+        self.assertTrue(raises)
+
+    def test_game_wrong_instance_append_monster(self):
+        raises = False
+        try:
+            game.append_monster('Monster')
+        except:
+            raises = True
+        self.assertTrue(raises)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
