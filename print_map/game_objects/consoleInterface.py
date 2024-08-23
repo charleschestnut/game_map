b40f97727f35a0ae19303f68927bf165177bd6f0
charleschestnut <carloscastanyo97@gmail.com>
2024-08-23 20:32:28 +0200

    feat#20: updated and reformat all the methods in ConsoleInterface

--- print_map/game_objects/consoleInterface.py
+++ print_map/game_objects/consoleInterface.py
@@ -2,6 +2,24 @@ import random
 from typing import List
 from .position import Position
 
+# Define constants for wall types
+EMPTY = 0
+MONSTER = 3
+START = 4
+PORTAL = 5
+FINISH = 6
+TREASURE = 7
+NORMAL_WALL = 1
+FAKE_WALL = 2
+
+# Define wall symbols
+NORMAL_WALL_SYMBOLS = [
+    '¤', '║', '═', '╔', '║', '║', '╚', '╠', '═', '╗', '═', '╦', '╝', '╣', '╩', '╬'
+]
+FAKE_WALL_SYMBOLS = [
+    'ø', '│', '-', '┌', '│', '│', '└', '├', '-', '┐', '-', '┬', '┘', '┤', '┴', '┼'
+]
+
 
 class ConsoleInterface:
 
@@ -89,41 +107,35 @@ class ConsoleInterface:
     # SQUARES CONSOLE
     @staticmethod
     def select_available_squares(squares):
-        str_input = ('Introduce the number of any of this position '
-                     'to move your character (from 0 to ') + str(len(squares) - 1) + '): \n'
+        def describe_square(index, square):
+            descriptions = {
+                0: "Square",
+                3: "There's a monster.",
+                4: "This is your start position of the game.",
+                5: "There's a portal.",
+                6: "YOU CAN WIN!",
+                7: "There's a 7."
+            }
+            return (f"Square number {index}: {square.position} - "
+                    f"{descriptions.get(square.type_square, 'Unknown type')}")
+
+        max_index = len(squares) - 1
+        str_input = (f'Introduce the number of any of this position to move '
+                     f'your character (from 0 to {str(len(squares) - 1)}) \n')
         acc = 0
         for square in squares:
-            if square.type_square == 0:
-                str_input += "Square number " + str(acc) + ": " + str(square.position) + ' \n'
-            elif square.type_square == 3:
-                str_input += ("Square number " + str(acc) + ": " + str(square.position) +
-                              " - There's a monster.\n")
-            elif square.type_square == 4:
-                str_input += ("Square number " + str(acc) + ": " + str(square.position) +
-                              " - This is your start position of the game\n")
-            elif square.type_square == 5:
-                str_input += ("Square number " + str(acc) + ": " + str(square.position) +
-                              " - There's a portal.\n")
-            elif square.type_square == 6:
-                str_input += ("Square number " + str(acc) + ": " + str(square.position) +
-                              " - YOU CAN WIN!.\n")
-            elif square.type_square == 7:
-                str_input += ("Square number " + str(acc) + ": " + str(square.position) +
-                              " - There's a 7.\n")
             acc += 1
+            str_input += describe_square(acc, square) + '\n'
 
-        index = input(str_input)
-        try:
-            index_int = int(index)
-            assert (0 <= index_int <= len(squares) - 1)
-            selected_square = squares[index_int]
-        except ValueError:
-            # Handle the case where index is not a valid integer
-            selected_square = ConsoleInterface.select_available_squares(squares)
-        except AssertionError:
-            # Handle the case where index_int is out of range
-            selected_square = ConsoleInterface.select_available_squares(squares)
-        return selected_square
+        # Input and validate user selection
+        while True:
+            index = input(str_input).strip()
+            if index.isdigit():
+                index_int = int(index)
+                if 0 <= index_int <= max_index:
+                    return squares[index_int]
+            print('Invalid input. Please enter a number between 0 and ', max_index)
+
 
     # BATTLE CONSOLE
     @staticmethod
@@ -144,8 +156,9 @@ class ConsoleInterface:
          ##    ##                                       #############################
         """
         print(battle_string)
+
         if has_won:
-            string = """
+            win_message = """
             ###     ###  ########  ########  ##########  ########  ########  ##    ##    ##
              ##     ##      ##     ##            ##      ##    ##  ##    ##   ##  ##     ##
               ##   ##       ##     ##            ##      ##    ##  ########    ####      ##
@@ -153,23 +166,21 @@ class ConsoleInterface:
                 ###      ########  ########      ##      ########  ##    ##     ##       ##
             """
 
-            print(string)
-            print('You have won in ' + str(acc) + ' iterations! Congratulations!\n'
-                                                  'Now you UPGRADED TO LEVEL ' + str(
-                battle.character.level) + '.\n'
-                                          'Your current status is:\n' + str(
-                battle.character.get_total_vital_status()) + '\n'
-                                                             'Your current position is ' + str(
-                battle.character.position))
+            print(win_message)
 
+            print(f'You have won in {str(acc)} iterations! Congratulations!\n'
+                  f'Now you UPGRADED TO LEVEL {str(battle.character.level)}.\n'
+                  f'Your current status is:\n'
+                  f'{str( battle.character.get_total_vital_status())} \n'
+                  f'Your current position is {str(battle.character.position)}')
         else:
-            string = 'You... HAVE LOST and HAVE BEEN REMOVED FROM THE GAME!!! \n' \
-                     "MUAHAHAHAHAHAHAHA!!!!! \n" \
-                     "...\n" \
-                     "I mean... You have been defeated, good luck next time!\n" \
-                     "PS: You have lost in only " + str(acc) + " interactions..\n." \
-                     "MY GRANNY WOULD SURVIVE BETTER THAN YOU MUAHAHAHAHA!!!!\n\n"
-            print(string)
+            lose_message = ('You... HAVE LOST and HAVE BEEN REMOVED FROM THE GAME!!! \n'
+                            "MUAHAHAHAHAHAHAHA!!!!! \n"
+                            "...\n"
+                            "I mean... You have been defeated, good luck next time!\n"
+                            "PS: You have lost in only {str(acc)} interactions..\n."
+                            "MY GRANNY WOULD SURVIVE BETTER THAN YOU MUAHAHAHAHA!!!!\n\n")
+            print(lose_message)
 
     # PORTAL
     @staticmethod
@@ -211,32 +222,29 @@ class ConsoleInterface:
     # WIN GAME
     @staticmethod
     def finish_game(game, winner):
-        string = ''
+        def format_loser_message(name):
+            return (f"Come on, {name}. How could you lose in that way?\n"
+                    "We're too embarrassed, really... Next time will be better...\n"
+                    "I guess...\n")
+
+        # Initialize the message
         if winner:
-            string = 'The winner is...' + str(winner.name.upper()) + '!!! \n' \
-                     "CON-GRA-TU-LA-TIONS!!!\n"\
-                     "But not to your mates, that were horrrible " \
-                     "playing, oh my godness...\n\n\n"
-            for character in game.characters:
-                if character.name != winner.name:
-                    c_name = character.name
-                    string += "Come on, " + str(c_name) + \
-                              ". How could you lose in that way?\n" \
-                              "We're too embarrased, really... Next time will be " \
-                              "better...\n" \
-                              "I guess...\n"
+            result_message = (f'The winner is... {winner.name.upper()}!!! \n'
+                              'CON-GRA-TU-LA-TIONS!!!\n'
+                              "But not to your mates, who were horrible at playing. "
+                              "Oh my goodness...\n\n\n")
+
+            # Append messages for all non-winners
+            result_message += ''.join(format_loser_message(character.name)
+                                      for character in game.characters
+                                      if character.name != winner.name)
 
         else:
-            for character in game.characters:
-                c_name = character.name
-                string = "Come on, " + str(c_name) + \
-                         ". How could you lose in that way?\n" \
-                         "We're too embarrased, really... Next time will be better...\n" \
-                         "I guess...\n"
-        string += "\n\n\n" \
-                  "I hope that you enjoyed that demo!\n" \
-                  "See you all soon!"
-        print(string)
+            result_message = ''.join(format_loser_message(character.name)
+                                     for character in game.characters)
+
+        result_message += "\n\n\n I hope that you enjoyed this demo!\nSee you all soon!"
+        print(result_message)
 
 
 def print_character_position(printed_map, character_position: Position):
@@ -265,109 +273,43 @@ def square_to_string(boardmap, numeric_square_list, index):
 
 
 def is_wall(boardmap, numeric_square_list, x, y, is_actual_square):
-    if x == -1 or x >= boardmap.rows:
+    if x < 0 or x >= boardmap.rows or y < 0 or y >= boardmap.cols:
         return 0
-    if y == -1 or y >= boardmap.cols:
-        return 0
+
     index = boardmap.rows * y + x
-    # El valor 1 será para las paredes normales, el 2 es para las paredes fakes
-    if numeric_square_list[index] == 1 or numeric_square_list[index] == 2:
-        if is_actual_square:
-            return numeric_square_list[index]
-        else:
-            return 1
-    else:
-        if is_actual_square:
-            return numeric_square_list[index]
-        else:
-            return 0
+    square_value = numeric_square_list[index]
+
+    if square_value in (NORMAL_WALL, FAKE_WALL):
+        return square_value if is_actual_square else NORMAL_WALL
+    return square_value if is_actual_square else EMPTY
 
 
 def get_wall_type(actual, left, top, right, bottom):
-    if actual == 0:  # Empty
+    if actual == EMPTY:
         return ' '
-    elif actual == 3:  # Monster
+    elif actual == MONSTER:
         return 'M'
-    elif actual == 4:  # Start point
+    elif actual == START:
         return 'S'
-    elif actual == 5:  # PORTAL
+    elif actual == PORTAL:
         return 'P'
-    elif actual == 6:  # FINISH POINT
+    elif actual == FINISH:
         return 'F'
-    elif actual == 7:  # TREASURE
+    elif actual == TREASURE:
         return 'T'
-
-    elif actual == 1:
-        return get_normal_wall([left, top, right, bottom])
-    else:  # Fake wall -> actual == 2
-        return get_fake_wall([left, top, right, bottom])
-
-
-def get_normal_wall(wall_binary_list):
-    if wall_binary_list == [0, 0, 0, 0]:
-        return '¤'
-    elif wall_binary_list == [0, 0, 0, 1]:
-        return '║'
-    elif wall_binary_list == [0, 0, 1, 0]:
-        return '═'
-    elif wall_binary_list == [0, 0, 1, 1]:
-        return '╔'
-    elif wall_binary_list == [0, 1, 0, 0]:
-        return '║'
-    elif wall_binary_list == [0, 1, 0, 1]:
-        return '║'
-    elif wall_binary_list == [0, 1, 1, 0]:
-        return '╚'
-    elif wall_binary_list == [0, 1, 1, 1]:
-        return '╠'
-    elif wall_binary_list == [1, 0, 0, 0]:
-        return '═'
-    elif wall_binary_list == [1, 0, 0, 1]:
-        return '╗'
-    elif wall_binary_list == [1, 0, 1, 0]:
-        return '═'
-    elif wall_binary_list == [1, 0, 1, 1]:
-        return '╦'
-    elif wall_binary_list == [1, 1, 0, 0]:
-        return '╝'
-    elif wall_binary_list == [1, 1, 0, 1]:
-        return '╣'
-    elif wall_binary_list == [1, 1, 1, 0]:
-        return '╩'
-    else:
-        return '╬'
+    elif actual == NORMAL_WALL:
+        return get_wall_symbol(left, top, right, bottom, NORMAL_WALL_SYMBOLS)
+    else:  # FAKE_WALL
+        return get_wall_symbol(left, top, right, bottom, FAKE_WALL_SYMBOLS)
 
 
-def get_fake_wall(wall_binary_list):
-    if wall_binary_list == [0, 0, 0, 0]:
-        return 'ø'
-    elif wall_binary_list == [0, 0, 0, 1]:
-        return '│'
-    elif wall_binary_list == [0, 0, 1, 0]:
-        return '-'
-    elif wall_binary_list == [0, 0, 1, 1]:
-        return '┌'
-    elif wall_binary_list == [0, 1, 0, 0]:
-        return '│'
-    elif wall_binary_list == [0, 1, 0, 1]:
-        return '│'
-    elif wall_binary_list == [0, 1, 1, 0]:
-        return '└'
-    elif wall_binary_list == [0, 1, 1, 1]:
-        return '├'
-    elif wall_binary_list == [1, 0, 0, 0]:
-        return '-'
-    elif wall_binary_list == [1, 0, 0, 1]:
-        return '┐'
-    elif wall_binary_list == [1, 0, 1, 0]:
-        return '-'
-    elif wall_binary_list == [1, 0, 1, 1]:
-        return '┬'
-    elif wall_binary_list == [1, 1, 0, 0]:
-        return '┘'
-    elif wall_binary_list == [1, 1, 0, 1]:
-        return '┤'
-    elif wall_binary_list == [1, 1, 1, 0]:
-        return '┴'
-    else:
-        return '┼'
+def get_wall_symbol(left, top, right, bottom, symbols):
+    wall_binary_list = [left, top, right, bottom]
+    wall_index = wall_binary_list_to_index(wall_binary_list)
+    return symbols[wall_index]
+
+
+def wall_binary_list_to_index(binary_list):
+    binary_string = ''.join(map(str, binary_list))
+    binary_index = int(binary_string, 2)
+    return binary_index if binary_index < len(NORMAL_WALL_SYMBOLS) else 0
