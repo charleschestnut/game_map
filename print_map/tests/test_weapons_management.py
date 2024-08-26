73eceacff9fe9462258c7e831e528ae3b26fd656
charleschestnut <carloscastanyo97@gmail.com>
2024-08-26 18:16:17 +0200

    feat#30: updated unit testing for name and description. Next step: create weapons with specialties

--- print_map/tests/test_weapons_management.py
+++ print_map/tests/test_weapons_management.py
@@ -2,34 +2,44 @@ import unittest
 from print_map.game_objects import Weapon
 from print_map.game_objects import VitalStatus
 
+# Constants
+STATUS_PROPORTION = [0.5, 0.5, 0.7, 0.9, 0.8]
+V_STATUS_NORMAL = VitalStatus(10, 10, 10, 10, 10, STATUS_PROPORTION)
+V_STATUS_NORMAL_POSITIVE = VitalStatus(10, 10, 10, 10, 10, STATUS_PROPORTION, 1)
+V_STATUS_NEGATIVE = VitalStatus(10, 10, 10, 10, 10, STATUS_PROPORTION, -1)
 
-status_proportion = [0.5, 0.5, 0.7, 0.9, 0.8]
-v_status_normal = VitalStatus(10, 10, 10, 10, 10, status_proportion)
-v_status_normal_positive = VitalStatus(10, 10, 10, 10, 10, status_proportion, 1)
-v_status_negative = VitalStatus(10, 10, 10, 10, 10, status_proportion, -1)
-
-name_weapon_1 = "Super Sword"
-name_weapon_2 = "Super Shield"
-description_weapon_1 = "This sword can cut everything"
-description_weapon_2 = "This shield can repel all the attacks"
+NAME_WEAPON_1 = "Super Sword"
+NAME_WEAPON_2 = "Super Shield"
+NAME_LONGER = "Super Ultra Mega Hero of all the Times"
+DESCRIPTION_WEAPON_1 = "This sword can cut everything"
+DESCRIPTION_WEAPON_2 = "This shield can repel all the attacks"
+DESCRIPTION_LONG = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod "
+                    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,"
+                    " quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.")
 
 
 class MyTestCase(unittest.TestCase):
     def test_create_single_weapon(self):
-        weapon = Weapon(name_weapon_1, description_weapon_1, v_status_normal)
+        weapon = Weapon(NAME_WEAPON_1, DESCRIPTION_WEAPON_1, V_STATUS_NORMAL)
         self.assertIsNotNone(weapon)
 
     def test_create_short_name(self):
-        self.assertRaises(Exception, Weapon, '.', description_weapon_1, v_status_normal)
+        self.assertRaises(ValueError, Weapon, '.', DESCRIPTION_WEAPON_1, V_STATUS_NORMAL)
+
+    def test_create_long_name(self):
+        self.assertRaises(ValueError, Weapon, NAME_LONGER, DESCRIPTION_WEAPON_1, V_STATUS_NORMAL)
+
+    def test_create_long_description(self):
+        self.assertRaises(ValueError, Weapon, NAME_WEAPON_1, DESCRIPTION_LONG, V_STATUS_NORMAL)
 
     def test_create_short_description(self):
-        self.assertRaises(Exception, Weapon, name_weapon_1, '.....', v_status_normal)
+        self.assertRaises(ValueError, Weapon, NAME_WEAPON_1, ".......", V_STATUS_NORMAL)
 
     def test_create_none_v_status(self):
-        self.assertRaises(Exception, Weapon, name_weapon_1, description_weapon_1, None)
+        self.assertRaises(ValueError, Weapon, NAME_WEAPON_1, DESCRIPTION_WEAPON_1, None)
 
     def test_create_v_status_wrong_instance(self):
-        self.assertRaises(Exception, Weapon, name_weapon_1, description_weapon_1,
+        self.assertRaises(ValueError, Weapon, NAME_WEAPON_1, DESCRIPTION_WEAPON_1,
                           "I'm not a v_status")
 
 
